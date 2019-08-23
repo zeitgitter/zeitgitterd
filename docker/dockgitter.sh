69393d4bff3d108d118b4e67722fe0e2bbbef4f0
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2019-08-23 15:15:53 +0200

    Fixe paths

--- docker/dockgitter.sh
+++ docker/dockgitter.sh
@@ -2,8 +2,9 @@
 # If config file has not yet been set up, create it and exit with message.
 # Otherwise, launch daemon
 if [ ! -f /var/lib/zeitgitter/zeitgitter.conf ]; then
-	cp /etc/zeitgitter.conf /ar/lib/zeitgitter/
-	echo "Please configure /var/lib/zeitgitter.conf first (everything marked REQUIRED)" >&2
+	cp /etc/zeitgitter.conf /var/lib/zeitgitter/
+	echo "*** Please configure /var/lib/zeitgitter.conf first."
+	echo "*** For a first run, edit everything kmarked REQUIRED."
 	exit 1
 fi
 exec /usr/local/sbin/zeitgitterd.py
