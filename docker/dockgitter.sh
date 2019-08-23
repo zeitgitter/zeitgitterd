5e6ba3bc426f5353f434e7f8f146e68ec602987c
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2019-08-23 15:26:25 +0200

    Move executables to /

--- docker/dockgitter.sh
+++ docker/dockgitter.sh
@@ -4,7 +4,7 @@
 if [ ! -f /var/lib/zeitgitter/zeitgitter.conf ]; then
 	cp /etc/zeitgitter.conf /var/lib/zeitgitter/
 	echo "*** Please configure /var/lib/zeitgitter.conf first."
-	echo "*** For a first run, edit everything kmarked REQUIRED."
+	echo "*** For a first run, edit everything marked REQUIRED."
 	exit 1
 fi
-exec /usr/local/sbin/zeitgitterd.py
+exec /zeitgitterd.py -c /var/lib/zeitgitter/zeitgitter.conf
