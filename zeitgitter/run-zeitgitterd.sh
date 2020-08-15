02dbb13126e48573b4e59ca6700eca8171224345
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2020-08-15 14:31:43 +0200

    :pencil: Be more verbose

--- zeitgitter/run-zeitgitterd.sh
+++ zeitgitter/run-zeitgitterd.sh
@@ -2,11 +2,11 @@
 # Docker runner
 
 # This needs to go *into* the volume, which is probably mapped by
-# docker-compose. So at start only…
+# docker-compose. So at start only… (as opposed to image creation)
 if [[ ! -d /persistent-data/.gnupg/ ]]; then
   gpg1 --pgp2 --import /root/stamper.asc
 fi
 if [[ ! -f /persistent-data/zeitgitter.conf ]]; then
-  cp /root/sample.conf /persistent-data/
+  cp --verbose /root/sample.conf /persistent-data/
 fi
 exec zeitgitterd "$@"
