4cfe01c5d967c46a079c9b8a6d5394ab17bfe67f
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2020-10-13 16:35:01 +0200

    :sparkles: Allow checking for timestamps repo health

---
+++ tools/zeitgitter-repo-health.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Check for healthiness of the repository.
+# Usage:
+#   check-repo-healthy.sh <repo-dir> <repo-url> <branch> <max-age>
+# - repo-dir: The directory to check the repository out in.
+# - repo-url: The URL to pull from.
+#             Will only be used if <repo-dir> does not yet exist.
+# - branch:   The branch to check for recent commits.
+#             Use branch names such as "origin/master" instead of "master".
+# - max-age:  The maximum age of the most recent commit in <branch>.
+#             Use "5 minutes ago" etc. (requires quotes)
+#
+# Make sure the current user has read access to the repository. This means that
+# - the remote repository needs to be public,
+# - there is a valid ssh key for that repository in ~/.ssh, or
+# - (not recommended) the credentials are part of the URL.
+#
+# Exit code (compatible with [Vigil](https://github.com/valeriansaliou/vigil#how-can-i-create-script-probes)):
+# - 0: Healthy
+# - 1: Sick (not used right now)
+# - >=2: Dead
+#
+# Example:
+#   ./check-repo-healthy.sh /tmp/github-gitta \
+#	https://github.com/zeitgitter/gitta-timestamps.git \
+#	origin/master \
+#	"1 hour ago"
+
+mkdir -p "$1"
+cd "$1" || exit 2
+
+# Pull from repository
+if [[ -r ".git" ]]; then
+  git fetch -q || exit 2
+else
+  git clone -q "$2" . || exit 2
+fi
+
+# Check whether there have been updates in the specified time window
+if [[ $(git log --since "$4" "$3" | wc -l) -ne 0 ]]; then
+  # At least one log entry in the time frame
+  exit 0
+else
+  echo "No commit to $2 $3 since $4" >&2
+  exit 2
+fi
