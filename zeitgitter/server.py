c0f5c02a7007cc5328cb1b26c2c32d9ae31f65e0
Marcel Waldvogel <marcel.waldvogel@trifence.ch>
2021-11-11 09:43:37 +0100

    🎨 No need for narrow regexp (DRY)
    Will be dealt with by `mimemap` test below
    
    Drive-by: The dot before the extension was unescaped

--- zeitgitter/server.py
+++ zeitgitter/server.py
@@ -114,7 +114,7 @@ class FlatFileRequestHandler(BaseHTTPRequestHandler):
         if self.path == '/':
             self.send_file('text/html', 'index.html', replace=subst)
         else:
-            match = re.match('^/([a-z0-9][-_.a-z0-9]*).(html|css|txt|xml|js|png|jpe?g|svg)$',
+            match = re.match(r'^/([a-z0-9][-_.a-z0-9]*)\.([a-z]*)$',
                              self.path, re.IGNORECASE)
             mimemap = {
                 'html': 'text/html',
